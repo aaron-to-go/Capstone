ea2d9e074130af4562c458750afe17723fc19b87
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-06-22 16:41:53 +0200

    addapt frontend app.js test

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders String', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText("This is a website");
   expect(linkElement).toBeInTheDocument();
 });
