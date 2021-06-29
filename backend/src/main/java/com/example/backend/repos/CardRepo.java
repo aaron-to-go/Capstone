9537cd4640611a55c90106748e09e8b00940dbdc
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-06-29 12:34:47 +0200

    add backend endpoint & functionality

--- backend/src/main/java/com/example/backend/repos/CardRepo.java
+++ backend/src/main/java/com/example/backend/repos/CardRepo.java
@@ -4,6 +4,11 @@ import com.example.backend.model.Card;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CardRepo extends PagingAndSortingRepository<Card, String> {
+
+    List<Card> findAll();
+
 }
