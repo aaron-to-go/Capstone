8fff0d99478e0322da5159d00357364afdc68ea2
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-07-01 10:51:52 +0200

    Add Login Conditional Rendering only if config is set

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 
 
@@ -13,24 +13,22 @@ export default function LoginPage() {
     }, [])
 
     const parameter = {
-        scope:"https%3A//www.googleapis.com/auth/userinfo.profile",
-        access_type:"offline",
-        include_granted_scopes:"true",
-        response_type:"code",
-        state:"state_parameter_passthrough_value",
-        redirect_uri:config,
-        client_id:"41125772544-es4ec32mlpq30rq3i8r8q5q8b3b25r7e.apps.googleusercontent.com"
+        scope: "https%3A//www.googleapis.com/auth/userinfo.profile",
+        access_type: "offline",
+        include_granted_scopes: "true",
+        response_type: "code",
+        state: "state_parameter_passthrough_value"
     }
 
-    const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${parameter.scope}&access_typ=${parameter.access_type}&include_granted_scopes=${parameter.include_granted_scopes}&response_type=${parameter.response_type}&state=${parameter.state}&redirect_uri=${parameter.redirect_uri}&client_id=${parameter.client_id}`
-
-
-
-    return(
-        <a href={url}>
-            <button>
-                Login
-            </button>
-        </a>
+    return (
+        <div>
+            {config &&
+            <a href={`https://accounts.google.com/o/oauth2/v2/auth?scope=${parameter.scope}&access_typ=${parameter.access_type}&include_granted_scopes=${parameter.include_granted_scopes}&response_type=${parameter.response_type}&state=${parameter.state}&redirect_uri=${config.redirect_uri}&client_id=${config.client_id}`}>
+                <button>
+                    Login
+                </button>
+            </a>
+            }
+        </div>
     )
 }
