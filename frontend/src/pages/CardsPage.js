ad8d1fefbcce583355aa0c98f9b53ebbd5aaa912
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-07-07 00:37:43 +0200

    Style CardPage

--- frontend/src/pages/CardsPage.js
+++ frontend/src/pages/CardsPage.js
@@ -1,18 +1,29 @@
 import CardItem from "../components/CardItem";
 import useCards from "../hooks/useCards";
+import {Container, Grid} from "@material-ui/core";
+import useStylesHook from "../hooks/useStyles";
+import NewCardButton from "../components/NewCardButton";
 
 export default function CardsPage(){
 
     const {cards} = useCards();
+    const {useStyles} = useStylesHook();
+    const classes = useStyles()
 
     return (
-        <div>
-            {cards.map((card) => (
-                <CardItem title={card.title}
-                    // state={card.state}
-                          description={card.description}
-                />
-            ))}
+        <div className={classes.heroContent}>
+            <Container className={classes.cardGrid} maxWidth="md">
+                <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card.id} xs={12} sm={6} md={4}>
+                        <CardItem title={card.title}
+                            description={card.description}
+                        />
+                        </Grid>
+                    ))}
+                </Grid>
+            </Container>
+            <NewCardButton/>
         </div>
     );
 }
