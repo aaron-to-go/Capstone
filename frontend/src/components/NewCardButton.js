d681763877c6f91dcef5e5fe90cbbc193c27c1d1
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-07-07 00:37:25 +0200

    Style AddNewCardButton

--- frontend/src/components/NewCardButton.js
+++ frontend/src/components/NewCardButton.js
@@ -1,9 +1,24 @@
-import {Link} from "react-router-dom";
+import {useHistory} from "react-router-dom";
+import {Fab} from "@material-ui/core";
+import AddIcon from '@material-ui/icons/Add';
+import useStylesHook from "../hooks/useStyles";
 
 export default function NewCardButton() {
 
-    return (
-        <Link to={"/new"}>New Card</Link>
-        );
+    const history = useHistory();
+    const routeChange = () =>{
+        let path = `/new`;
+        history.push(path);
+    }
 
+    const {useStyles} = useStylesHook();
+    const classes = useStyles()
+
+    return (
+
+        <Fab  color="primary" aria-label="add"
+              className={classes.addButton}>
+            <AddIcon onClick={routeChange}/>
+        </Fab>
+        );
     }
