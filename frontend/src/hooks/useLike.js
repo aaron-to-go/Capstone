3fab48b3e599aac644e6f6ba90b00ade36d2add9
Aaron Dutschmann <aaron@dutschmann-online.de>
2021-07-08 21:57:28 +0200

    add useLike custom Hook with axios request

---
+++ frontend/src/hooks/useLike.js
@@ -0,0 +1,26 @@
+import {useContext, useState} from "react";
+import AuthContext from "../context/AuthContext";
+import axios from "axios";
+
+export default function useLike(initialState, card) {
+
+    const [like, setLike] = useState(initialState);
+    const {token} = useContext(AuthContext);
+
+    function handleClick(event) {
+        event.preventDefault()
+        console.log("hook triggered")
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        }
+        axios
+            .put(`/api/cards/${card.id}`,{} ,config)
+            .then((response) => response.data)
+            .then(setLike)
+            .catch((error) => console.error(error.message));
+    }
+
+    return {like, handleClick};
+}
